353fdfa74c85fbb222fe6b357d51288b2c4be588
jeffrey-tafe <141377378+jeffrey-tafe@users.noreply.github.com>
2023-09-20 13:10:59 +0930

    Update ArrayDemo.java

--- session08/src/main/java/com/mycompany/session08/ArrayDemo.java
+++ session08/src/main/java/com/mycompany/session08/ArrayDemo.java
@@ -22,25 +22,6 @@ public class ArrayDemo {
 
         Scanner kbd = new Scanner(System.in);
 
-//      options to declare and create the array
-//      declare variable first
-//      int marks[];
-//      then create array
-//      marks = new int[MAX];
-//      then set the value (mark in this case) of each element
-//
-//      or declare the variable and create the array all in one line
-//      int marks[] = new int[MAX];
-//
-//      or you can put the square brackets after the datatype
-//      int[] marks = new int[MAX];
-//
-//      or 
-//      declare variable and define size by defining the data, everything is happening at once here
-//      including the size of the array through the number of data items given (5 in this case)
-//      int[] marks= {10,11,10,10,12};
-        
-        //Lets do it this way
         int marks[] = new int[MAX];
 
         // You can set the data for each element of the array using the index in the square brackets 
@@ -54,6 +35,19 @@ public class ArrayDemo {
         // print out all the marks
         // The array now knows how much data it can take through the use of - array.length
         System.out.println("\nPrinting all the marks set in the code:");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+
+        //Now lets get the user to enter the marks
+        //input students marks
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Enter the students mark :");
+            marks[i] = kbd.nextInt();
+        }
+        System.out.println("All marks have now been entered");
+        // print out all the marks
+        System.out.println("\nPrinting all the marks entered by the user:");
         for (int i = 0; i < marks.length; i++) {
             System.out.print(marks[i] + " ");
         }
