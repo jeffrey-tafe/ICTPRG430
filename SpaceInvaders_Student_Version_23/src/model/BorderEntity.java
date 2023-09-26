f2998d98115bebdbb1b15b8416b91f1b3738f8bf
jeffrey-tafe <141377378+jeffrey-tafe@users.noreply.github.com>
2023-09-26 21:05:48 +0930

    Add BorderEntity setters and getters

--- SpaceInvaders_Student_Version_23/src/model/BorderEntity.java
+++ SpaceInvaders_Student_Version_23/src/model/BorderEntity.java
@@ -9,7 +9,7 @@ import java.awt.Rectangle;
  * Extends Entity class with width, height and border id
  *
  * @author sruiz
- * @author jruiz 
+ * @author jruiz
  * @author Jeffrey Smith
  * @version 1.0 initial version
  * @version 2.0 updated to get better consistency in the order of parameters
@@ -29,33 +29,54 @@ public class BorderEntity extends Entity {
 
     //no args constructors
     public BorderEntity() {
-        //TODO
-
+        this(DEFAULT_ID);
     }
 
     public BorderEntity(String id) {
-        //TODO
-
+        this(id, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 
     public BorderEntity(String id, int width, int height) {
-        //TODO
-
+        this(id, DEF_X, DEF_Y, width, height);
     }
 
     public BorderEntity(int initalXPos, int initialYPos, int width, int height) {
-        //TODO
-
+        this(DEFAULT_ID, initalXPos, initialYPos, width, height);
     }
 
     //all args constructor
     public BorderEntity(String id, int initalXPos, int initialYPos, int width, int height) {
-        super(null, initalXPos, initialYPos);
-
+        super(DEF_REF_TO_SPRITE_IMAGE_FILE, initalXPos, initialYPos);
+        this.id = id;
+        this.width = width;
+        this.height = height;
     }
 
     //Getters and Setters
-    //TODO
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     //toString (no equals)
     @Override
     public String toString() {
