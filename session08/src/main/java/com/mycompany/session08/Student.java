0ef87b11a736dc23ff543c3ae1655c6564f5b41a
jeffrey-tafe <141377378+jeffrey-tafe@users.noreply.github.com>
2023-10-14 14:54:24 +1030

    Session 8 Exercise 8

--- session08/src/main/java/com/mycompany/session08/Student.java
+++ session08/src/main/java/com/mycompany/session08/Student.java
@@ -11,69 +11,97 @@ package com.mycompany.session08;
 public class Student extends Person {
 
 // Constants
-  public static final String DEFAULT_STUDENT_ID = "NOT DEFINED";
+    public static final String DEFAULT_STUDENT_ID = "NOT DEFINED";
+    public static final int DEF_NUM_SUBJECTS = 5;
 
-  // Instance Variables
-  private String studentID;
-  private Subject subject;
+    // Instance Variables
+    private String studentID;
+    private int numSubjects;
+    // student "has a" array of subjects
+    private Subject[] subjects;
 
-  // Constructors
-  public Student() {
-    this(DEFAULT_STUDENT_ID);
-  }
+    // Constructors
+    public Student(
+            String studentID,
+            String firstName,
+            String surname,
+            int age,
+            Address address,
+            int maxSubjects
+    ) {
+        super(firstName, surname, age, address);
+        this.studentID = studentID;
+        this.subjects = new Subject[maxSubjects];
+    }
 
-  public Student(String studentID) {
-    this(studentID, new Subject());
-  }
+    public Student(String studentID) {
+        this(
+                studentID,
+                DEFAULT_FIRST_NAME,
+                DEFAULT_LAST_NAME,
+                DEFAULT_AGE,
+                new Address(),
+                DEF_NUM_SUBJECTS);
+    }
 
-  public Student(String studentID, Subject subject) {
-    this(studentID, subject, DEFAULT_FIRST_NAME, DEFAULT_LAST_NAME,
-            DEFAULT_AGE, new Address());
-  }
+    public Student() {
+        this(DEFAULT_STUDENT_ID);
+    }
 
-  public Student(String studentID, String subjectCode, String subjectName) {
-    this(studentID, new Subject(subjectCode, subjectName));
-  }
+    // Methods
+    public String getStudentID() {
+        return studentID;
+    }
 
-  public Student(String studentID, Subject subject, String firstName,
-          String surname, int age, Address address) {
-    super(firstName, surname, age, address);
-    this.studentID = studentID;
-    this.subject = subject;
-  }
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
 
-  // Methods
-  public String getStudentID() {
-    return studentID;
-  }
+    public int getNumSubjects() {
+        return numSubjects;
+    }
 
-  public void setStudentID(String studentID) {
-    this.studentID = studentID;
-  }
+    public void setNumSubjects(int numSubjects) {
+        this.numSubjects = numSubjects;
+    }
 
-  public Subject getSubject() {
-    return subject;
-  }
+    public void addSubject(Subject subject) {
+        if (this.numSubjects < this.subjects.length) {
+            this.subjects[this.numSubjects] = subject;
+            this.numSubjects++;
+        }
+    }
 
-  public void setSubject(Subject subject) {
-    this.subject = subject;
-  }
+    public String getSubjects() {
+        StringBuilder sb = new StringBuilder();
+        
+        if(this.numSubjects < 1) {
+            return "NO SUBJECTS";
+        }
+        
+        for (int i = 0; i < numSubjects; i++) {
+            sb.append(subjects[i]);
+        }
+        return sb.toString();
+    }
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append(super.toString());
-    sb.append("\n[");
-    sb.append("\n\tstudentID: ");
-    sb.append(this.studentID);
-    sb.append("\n\tsubject: ");
-    sb.append(this.subject);
-    sb.append("\n]");
-    return sb.toString();
-  }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append("\n[");
+        sb.append("\n\tstudentID: ");
+        sb.append(this.studentID);
+        sb.append("\n\tnumSubjects: ");
+        sb.append(this.numSubjects);
+        sb.append("\n\tsubjects: ");
+        sb.append(this.getSubjects());
+        sb.append("\n]");
+        return sb.toString();
+    }
 
-  public String describeRole() {
-    return "Student studies";
-  }
+    public String describeRole() {
+        return "Student studies";
+    }
 
 }
