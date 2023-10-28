1a60a64089e083bf035eba93ddeef01a507a034d
jeffrey-tafe <141377378+jeffrey-tafe@users.noreply.github.com>
2023-10-28 16:16:12 +1030

    Update lab_08.java

--- session09/src/main/java/com/mycompany/session09/lab_08.java
+++ session09/src/main/java/com/mycompany/session09/lab_08.java
@@ -1,3 +1,5 @@
+package com.mycompany.session09;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
