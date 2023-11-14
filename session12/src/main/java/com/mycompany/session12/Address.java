5a4c8a804eacc33df76b51b2fbec9e3de772a877
jeffrey-tafe <141377378+jeffrey-tafe@users.noreply.github.com>
2023-11-14 14:29:26 +1030

    Session 12 Activities

---
+++ session12/src/main/java/com/mycompany/session12/Address.java
@@ -0,0 +1,128 @@
+package com.mycompany.session12;
+
+public class Address {
+
+  //constants
+  public static final String DEFAULT_STREET_NUMBER = "NOT DEFINED";
+  public static final String DEFAULT_STREET_NAME = "NOT DEFINED";
+  public static final String DEFAULT_SUBURB = "NOT DEFINED";
+  public static final String DEFAULT_STATE = "NOT DEFINED";
+  public static final String DEFAULT_COUNTRY = "NOT DEFINED";
+  public static final String DEFAULT_POST_CODE = "NOT DEFINED";
+
+  // instance variables
+  private String streetNumber;
+  private String streetName;
+  private String suburb;
+  private String state;
+  private String country;
+  private String postCode;
+
+  // all arg constructor
+  public Address(
+          String streetNumber,
+          String streetName,
+          String suburb,
+          String state,
+          String country,
+          String postCode) {
+    this.streetNumber = streetNumber;
+    this.streetName = streetName;
+    this.suburb = suburb;
+    this.state = state;
+    this.country = country;
+    this.postCode = postCode;
+  }
+
+// no arg constructor
+  public Address() {
+    this(
+            DEFAULT_STREET_NUMBER,
+            DEFAULT_STREET_NAME,
+            DEFAULT_SUBURB,
+            DEFAULT_STATE,
+            DEFAULT_COUNTRY,
+            DEFAULT_POST_CODE
+    );
+  }
+
+  // setters
+  public void setStreetNumber(String streetNumber) {
+    this.streetNumber = streetNumber;
+  }
+
+  public void setStreetName(String streetName) {
+    this.streetName = streetName;
+  }
+
+  public void setSuburb(String suburb) {
+    this.suburb = suburb;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public void setPostCode(String postCode) {
+    this.postCode = postCode;
+  }
+
+  // getters
+  public String getStreetNumber() {
+    return this.streetNumber;
+  }
+
+  public String getStreetName() {
+    return this.streetName;
+  }
+
+  public String getSuburb() {
+    return this.suburb;
+  }
+
+  public String getState() {
+    return this.state;
+  }
+
+  public String getCountry() {
+    return this.country;
+  }
+
+  public String getPostCode() {
+    return this.postCode;
+  }
+
+  // toString method for debugging
+  @Override
+
+  public String toString() {
+
+    StringBuilder sb = new StringBuilder();
+    sb.append(super.toString());
+    sb.append("\n[");
+    sb.append("\n\t");
+    sb.append("streetNumber: ");
+    sb.append(this.streetNumber);
+    sb.append("\n\t");
+    sb.append("streetName: ");
+    sb.append(this.streetName);
+    sb.append("\n\t");
+    sb.append("suburb: ");
+    sb.append(this.suburb);
+    sb.append("\n\t");
+    sb.append("state: ");
+    sb.append(this.state);
+    sb.append("\n\t");
+    sb.append("country: ");
+    sb.append(this.country);
+    sb.append("\n\t");
+    sb.append("postCode: ");
+    sb.append(this.postCode);
+    sb.append("\n]");
+    return sb.toString();
+  }
+}
--- session12/src/main/java/com/mycompany/session12/PlayWithFiles.java
+++ session12/src/main/java/com/mycompany/session12/PlayWithFiles.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.session12;
 
+import java.io.*;
+import java.util.Scanner;
+
 /**
  *
  * @author jeffs
@@ -14,7 +17,140 @@ public class PlayWithFiles {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        try {
+            textOutputFile();
+            textInputFile1();
+            textInputFile2();
+            binaryOutputFile();
+            binaryInputFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
-    
+
+    public static void textOutputFile() throws IOException {
+        File theFile = new File("TestTextFile.txt");
+        FileWriter outFileWriter = new FileWriter(theFile);
+
+        // You can open a file to append text by using the FileWriter constructor
+        // that allows a second parameter indicating that you want to append.
+        // Otherwise a new file is created each time.
+        // FileWriter outFileWriter = new FileWriter(theFile, true);
+        PrintWriter out = new PrintWriter(outFileWriter);
+
+        out.println(12345);
+        out.println("Hello");
+        out.println(123.5);
+        out.println(false);
+
+        out.close();
+    }
+
+    /**
+     * This method demonstrated using the to read lines in a text file. It
+     * assumes the file is created with the method textOutputFile.
+     *
+     * @throws java.io.IOException
+     */
+    public static void textInputFile1() throws IOException {
+        BufferedReader in;
+        String inStr;
+        int anInt;
+        double aDouble;
+        boolean aBoolean;
+
+        System.out.println("===== EXAMPLE OF READING USING BUFFEREDREADER CLASS =======");
+        in = new BufferedReader(new FileReader(new File("TestTextFile.txt")));
+
+        inStr = in.readLine();
+        anInt = Integer.parseInt(inStr);
+        System.out.println(anInt);
+
+        inStr = in.readLine();
+        System.out.println(inStr);
+
+        inStr = in.readLine();
+        aDouble = Double.parseDouble(inStr);
+        System.out.println(aDouble);
+
+        inStr = in.readLine();
+        aBoolean = Boolean.parseBoolean(inStr);
+        System.out.println(aBoolean);
+
+        in.close();
+    }
+
+    public static void textInputFile2() throws IOException {
+        FileReader inFileReader;
+        Scanner in;
+        inFileReader = new FileReader("TestTextFile.txt");
+        in = new Scanner(inFileReader);
+        String inStr;
+        int anInt;
+        double aDouble;
+        boolean aBoolean;
+
+        System.out.println("=== EXAMPLE OF READING USING SCANNER CLASS ===");
+
+        anInt = in.nextInt();
+        System.out.println(anInt);
+
+        inStr = in.next();
+        System.out.println(inStr);
+
+        aDouble = in.nextDouble();
+        System.out.println(aDouble);
+
+        aBoolean = in.nextBoolean();
+        System.out.println(aBoolean);
+
+        in.close();
+    }
+
+    public static void binaryOutputFile() throws IOException {
+        DataOutputStream dos = new DataOutputStream(
+                new FileOutputStream("TestBinaryFile.bin"));
+
+        // Add a person object instance variables to the file
+        Person p = new Person("BOB", "DOWN", 22);
+
+        dos.writeUTF(p.getFirstName());
+        dos.writeUTF(p.getLastName());
+        dos.writeInt(p.getAge());
+
+        // Add some variables to the file
+        // Write how many ins wil follow, then write the ins
+        dos.writeInt(5);;
+        for (int i = 1; i <= 5; i++) {
+            dos.writeInt(i);
+        }
+        dos.close();
+
+    }
+
+    public static void binaryInputFile() throws IOException {
+        DataInputStream dis = new DataInputStream(
+                new FileInputStream("TestBinaryFile.bin"));
+        int items;
+        int n;
+
+        Person p = new Person();
+
+        p.setFirstName(dis.readUTF());
+        p.setLastName(dis.readUTF());
+        p.setAge(dis.readInt());
+
+        System.out.println("Person p from file = " + p);
+
+        // read how many integers will follow then read the integers
+        items = dis.readInt();
+        for (int i = 1; i <= items; i++) {
+            n = dis.readInt();
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        dis.close();
+    }
 }
